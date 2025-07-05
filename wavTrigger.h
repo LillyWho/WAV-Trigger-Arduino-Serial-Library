117485f991a0070c1e765c4581af6a9e651bd9e8
Lilly Evans <5427724+LillyWho@users.noreply.github.com>
2025-07-05 20:47:22 +0000

    Change serial selection mechanism to allow for falling back
    If the macros for hardware serial aren't set, fall back to softserial.
    If any one of them are set, don't set the softserial macro and don't include the softserial lib.

--- wavTrigger.h
+++ wavTrigger.h
@@ -34,10 +34,13 @@
 // The following defines are used to control which serial class is
 //  used. Uncomment only the one you wish to use. If all of them are
 //  commented out, the library will use Hardware Serial
-#define __WT_USE_ALTSOFTSERIAL__
+
 //#define __WT_USE_SERIAL1__
 //#define __WT_USE_SERIAL2__
 //#define __WT_USE_SERIAL3__
+#if !defined(__WT_USE_SERIAL1__) || !defined(__WT_USE_SERIAL2__) || !defined(__WT_USE_SERIAL3__)
+#define __WT_USE_ALTSOFTSERIAL__
+#endif
 // ==================================================================
 
 #define CMD_GET_VERSION					1
